2bce5628d669ecc9f57de973efa09377842d246a
kielx <kiel66@gmail.com>
2022-11-29 22:22:23 +0100

    Add 2 rollup query

--- SELECT.sql
+++ SELECT.sql
@@ -5,4 +5,18 @@ SELECT  EXTRACT(year FROM insurance.begin_date) AS Rok, region_id, employee_id, 
 FROM insurance
 INNER JOIN employee USING (employee_id)
 GROUP BY ROLLUP (EXTRACT(year FROM insurance.begin_date), region_id, employee_id)
-ORDER BY EXTRACT(year FROM insurance.begin_date), COUNT(employee_id) DESC
+ORDER BY EXTRACT(year FROM insurance.begin_date), COUNT(employee_id) DESC;
+
+
+-- 01 ROLLUP - Baza danych
+--Roczne zestawienie iloœci sprzedanych polis w odniesieniu do oddzia³u i typu polisy
+
+SELECT  EXTRACT(year FROM insurance.begin_date) AS Rok, branch.branch_id, insurancetype.insurance_type,  count(insurance.insuranceType_id) AS Ilosc
+FROM insurance
+INNER JOIN insuranceType ON insurance.insurancetype_id = insurancetype.insurancetype_id
+INNER JOIN branch ON insurance.branch_id = branch.branch_id
+GROUP BY ROLLUP (EXTRACT(year FROM insurance.begin_date), branch.branch_id, insurancetype.insurance_type )
+ORDER BY EXTRACT(year FROM insurance.begin_date), branch.branch_id, COUNT(insurance.insuranceType_id) DESC
+
+
+
